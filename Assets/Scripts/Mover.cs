eb7f3891df6715fb9a1b689cc5d01a3eba4f4d85
DESKTOP-155CKK6\EZEN <ashleyriot000@gmail.com>
2025-11-27 20:09:43 +0900

    유니티 물리 기초 추가

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Mover : MonoBehaviour
+{
+    public Transform forwardTransform;
+    public float moveSpeed = 3f;
+
+    private Rigidbody _rigidBody;
+    private Vector2 _direction;
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        _direction = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 forward = forwardTransform.forward;
+        Vector3 right = forwardTransform.right;
+        forward.y = 0f;
+        forward.Normalize();
+        right.y = 0f;
+        right.Normalize();
+
+        Vector2 direction = moveSpeed * Time.fixedDeltaTime * _direction;
+        Vector3 newPos = (direction.y * forward) + (direction.x * right) + _rigidBody.position;
+        _rigidBody.MovePosition(newPos);
+    }
+
+    //private void Update()
+    //{
+    //    Vector3 forward = forwardTransform.forward;
+    //    Vector3 right = forwardTransform.right;
+    //    forward.y = 0f;
+    //    forward.Normalize();
+    //    right.y = 0f;
+    //    right.Normalize();
+
+    //    Vector2 direction = moveSpeed * Time.deltaTime * _direction;
+    //    Vector3 newPos = (direction.y * forward) + (direction.x * right);
+    //    transform.Translate(newPos, Space.World);
+    //}
+
+}
