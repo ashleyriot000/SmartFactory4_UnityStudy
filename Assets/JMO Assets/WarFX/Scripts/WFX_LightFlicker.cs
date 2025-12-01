631836db7230f9a32f19df1ac5ea0872516ab8a3
DESKTOP-155CKK6\EZEN <ashleyriot000@gmail.com>
2025-12-01 18:54:25 +0900

    물리 응용 슈팅 예제 완료

--- Assets/JMO Assets/WarFX/Scripts/WFX_LightFlicker.cs
+++ Assets/JMO Assets/WarFX/Scripts/WFX_LightFlicker.cs
@@ -10,29 +10,22 @@ using System.Collections;
 [RequireComponent(typeof(Light))]
 public class WFX_LightFlicker : MonoBehaviour
 {
-	public float time = 0.05f;
-	
-	private float timer;
-	
-	void Start ()
+    private Light _fireLight;
+
+    private void Awake()
+    {
+        _fireLight = GetComponent<Light>();
+    }
+    void OnEnable ()
 	{
-		timer = time;
 		StartCoroutine("Flicker");
 	}
 	
 	IEnumerator Flicker()
 	{
-		while(true)
-		{
-			GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
-			
-			do
-			{
-				timer -= Time.deltaTime;
-				yield return null;
-			}
-			while(timer > 0);
-			timer = time;
-		}
+        _fireLight.enabled = true;
+        yield return null;
+        yield return null;
+        _fireLight.enabled = false;
 	}
 }
